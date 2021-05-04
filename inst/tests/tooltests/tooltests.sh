5e5afbcdbbabbc32968aab0a07b14d108a4c6ce3
Sean <maden@ohsu.edu>
2021-05-04 15:50:01 -0700

    Create tooltests.sh

---
+++ inst/tests/tooltests/tooltests.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+
+# Author: Sean Maden
+#
+# This script tests each of the retained intron analysis tools using example
+# BAM .bam and .bai files provided in the `IntEREst` package.
